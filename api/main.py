5df96963eeca849c2a434424c731f7a0f6bb69ad
Alex <63930909+alexdzm@users.noreply.github.com>
2023-04-21 21:15:32 +0100

    added cat and desc api endpoints

--- api/main.py
+++ api/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+import os
+import openai
 
 app = FastAPI()
 
@@ -6,3 +8,39 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get('/category/new')
+async def make_new_category(categories:list)->list:
+    template=gen_template(categories,mode='category')
+    response=call_openai(template)
+    new_categories=parse_response(response)
+    return new_categories
+
+@app.get('/description/new')
+async def make_new_description(categories:list)->list:
+    raise NotImplementedError
+
+#------------------HELPER FUNCTIONS------------------#
+
+
+def gen_template(categories,mode:str)->str:
+    raise NotImplementedError
+
+def call_openai(prompt:str,model:str="text-davinci-003"):
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+
+    response = openai.Completion.create(
+    model=model,
+    prompt=prompt,
+    temperature=0,
+    max_tokens=64,
+    top_p=1.0,
+    frequency_penalty=0.0,
+    presence_penalty=0.0,
+    stop=["\"\"\""]
+    )
+
+    return response
+
+def parse_response(response)->list:
+    raise NotImplementedError
